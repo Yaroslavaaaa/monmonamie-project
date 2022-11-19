7b98068e43953501704b75069a0abd63dae4c494
Yaroslavaa <104118022+Yaroslavaa@users.noreply.github.com>
2022-11-19 15:01:53 +0600

    Initial commit

---
+++ src/main/java/kz/narxoz/monamieproject/MonamieProjectApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.monamieproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonamieProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MonamieProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kz/narxoz/monamieproject/MonamieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.monamieproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonamieProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
